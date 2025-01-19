704b062ac7efffda69c859af474f311c524d7667
Hufeisen <hufeisen@hufeisen-games.de>
2025-01-19 14:23:59 +0100

    feat: listener and setup command

---
+++ src/main/java/dev/hufeisen/maedn/utils/ArmorStandUtils.java
@@ -0,0 +1,31 @@
+package dev.hufeisen.maedn.utils;
+
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+
+public class ArmorStandUtils {
+
+    public static ArmorStand spawnAmorStand(Location location, Color color) {
+
+        ArmorStand armorStand = location.getWorld().spawn(location.add(0, 2, 0).toCenterLocation(), ArmorStand.class);
+
+        armorStand.getEquipment().setHelmet(generateArmor(Material.LEATHER_HELMET, color));
+        armorStand.getEquipment().setChestplate(generateArmor(Material.LEATHER_CHESTPLATE, color));
+        armorStand.getEquipment().setLeggings(generateArmor(Material.LEATHER_LEGGINGS, color));
+        armorStand.getEquipment().setBoots(generateArmor(Material.LEATHER_BOOTS, color));
+        return armorStand;
+    }
+
+    private static ItemStack generateArmor(Material material, Color color) {
+        ItemStack leatherArmor = new ItemStack(material);
+        LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) leatherArmor.getItemMeta();
+        leatherArmorMeta.setColor(color);
+        leatherArmor.setItemMeta(leatherArmorMeta);
+        return leatherArmor;
+    }
+
+}
